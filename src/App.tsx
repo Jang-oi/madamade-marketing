aef619fa53e6664b1b863269d61b5ec43ee3eebe
Jang-oi <jjh53241@gmail.com>
2024-02-20 22:47:56 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/joy';
+import {Route, Routes, useNavigate} from 'react-router-dom';
+import {Box} from '@mui/joy';
 import Header from './components/common/Header';
 import NotFound from './pages/NotFound';
 import Main from './pages/Main';
 import MadaSnackbar from './components/common/MadaSnackbar';
 import License from "./pages/License";
+import React, {useEffect, useState} from "react";
+import {axiosAPI} from "./utils/axios";
+import LoadingComponent from "./components/common/LoadingComponent";
 
 const App = () => {
+
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        setIsLoading(true);
+        axiosAPI('/getLicense', {})
+            .then((res) => {
+                const response = res.data;
+                const {returnCode} = response;
+
+                if (returnCode > 0) navigate('/');
+                else navigate('/license');
+            })
+            .catch()
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    if (isLoading) return <LoadingComponent/>;
 
     return (
         <>
--- src/pages/License.tsx
+++ src/pages/License.tsx
@@ -1,8 +1,13 @@
 import ErrorView from '../components/common/ErrorView';
-import {openNewTab} from "../utils/commonUits";
+import {axiosAPI} from "../utils/axios";
 
 const License = () => {
-    return <ErrorView code={'라이센스 갱신이 필요합니다.'} buttonValue={'갱신하기'} buttonCallBack={() => {openNewTab('https://thoracic-spring-58d.notion.site/29bb6d7c62584007ad8fa895f5e89973')}}/>;
+
+    const onLicenseButtonHandler = async () => {
+        await axiosAPI('/setLicense', {});
+    }
+
+    return <ErrorView code={'라이센스 갱신이 필요합니다.'} buttonValue={'갱신하기'} buttonCallBack={onLicenseButtonHandler}/>;
 };
 
 export default License;
